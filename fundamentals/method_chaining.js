e164fb2c7614a4732af59a7ec79e64ae96a03541
yogiifr <yogiffrr@gmail.com>
2024-02-09 23:50:23 +0700

    feat: learned combo on function with method chaining

---
+++ fundamentals/method_chaining.js
@@ -0,0 +1,19 @@
+// Without Chaining
+
+let username = window.prompt("Enter your username:")
+
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+
+username = letter + extraChars;
+
+console.log(username);
+
+// Method Chaining
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
