99dd4d4f32bc22741dc3741d361710ade93990c1
yogiifr <yogiffrr@gmail.com>
2024-02-12 06:27:10 +0700

    feat: learn a async code

---
+++ fundamentals/async.js
@@ -0,0 +1,16 @@
+// Handles with callbacks, promises, async/await
+
+function func1(callback){
+    setTimeout(() => {
+        console.log("Task 1");
+        callback();
+    }, 3000);
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
