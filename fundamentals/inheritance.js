591d7053ef2514e81449dd1fbf2391fa00b67ad0
yogiifr <yogiffrr@gmail.com>
2024-02-11 17:56:04 +0700

    feat: learn inheritence of class with extends

---
+++ fundamentals/inheritance.js
@@ -0,0 +1,43 @@
+class Animal{
+    alive = true;
+
+    eat(){
+        console.log(`This ${this.name} is eating`);
+    }
+
+    sleep(){
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal{
+    name = "rabbit";
+
+    run(){
+        console.log(`This ${this.name} is running`)
+    }
+}
+
+class Fish extends Animal{
+    name = "fish";
+
+    swim(){
+        console.log(`This ${this.name} is swimming`)
+    }
+}
+
+class Hawk extends Animal{
+    name = "hawk";
+
+    fly(){
+        console.log(`This ${this.name} is flying`)
+    }
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+rabbit.eat();
+
+fish.swim();
