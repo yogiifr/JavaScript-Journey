5a455a66d82947df7622797b9a8a72419d13b5f1
yogiifr <yogiffrr@gmail.com>
2024-02-11 21:42:28 +0700

    feat: learn what is array object

---
+++ fundamentals/array_object.js
@@ -0,0 +1,6 @@
+const fruits = [
+    {name: "apple", color: "red", caloreis: 95},
+    {name: "orange", color: "orange", caloreis: 45},
+    {name: "banana", color: "yellow", caloreis: 105},
+    {name: "coconut", color: "white", caloreis: 159},
+                ]
