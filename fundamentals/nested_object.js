33216473bfc9996e342affcd6e0699e12a49a269
yogiifr <yogiffrr@gmail.com>
2024-02-11 21:36:22 +0700

    feat: learn to make object on object with nest

---
+++ fundamentals/nested_object.js
@@ -0,0 +1,18 @@
+
+const person = {
+    fullName: "Spongebob Squarepants",
+    age: 30,
+    isStudent: true,
+    hobbies: ["karate", "jellyfishing", "cooking"],
+    address: {
+        street: "124 Conch St.",
+        city: "Bikini Bottom",
+        country: "Int. Water"
+    }
+}
+
+console.log(person.fullName);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.hobbies[0]);
+console.log(person.address.street);
