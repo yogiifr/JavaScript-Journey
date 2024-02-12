2ec8de8ff09f21b038d683ab7b20f0ad279d7e83
yogiifr <yogiffrr@gmail.com>
2024-02-12 10:48:14 +0700

    feat: learn navigation on DOM

---
+++ fundamentals/dom_navigation.js
@@ -0,0 +1,9 @@
+const element = document.getElementById("fruits");
+const firstChild = element.firstElementChild;
+
+// element.firstElementChild
+// element.lastElementChild
+// element.nextElementSibling
+// element.previousElementSibling
+// element.parentElement
+// element.children
