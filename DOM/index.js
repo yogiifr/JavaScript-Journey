af53fa3ecc3c279643070690de8ee31b7aeabbf4
yogiifr <yogiffrr@gmail.com>
2024-02-13 07:53:06 +0700

    fix: move some to fundamentals

--- DOM/index.js
+++
@@ -1,8 +0,0 @@
-// STOPWATCH PROGRAM
-
-const username = "Yogi";
-const welcoming = document.getElementById("welcome-msg");
-
-welcoming.textContent += username === "" ? 'Guest' : username;
-
-console.dir(document);
